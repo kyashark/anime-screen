f5152d72ddfa25ba206ece849ebe6cc5dd27a4f7
shakya <152844822+kyashark@users.noreply.github.com>
2025-01-20 01:16:00 +0530

    Implement Navbar

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+// Hamburger Menu
+
+const menuItem = document.getElementById("menu-item");
+if(menuItem){
+    menuItem.addEventListener('click',()=>{
+        document.getElementById("nav-bar").classList.toggle("show");
+        document.querySelectorAll("span").forEach((bar)=>{
+            bar.classList.toggle("change");
+        });
+    });
+}
