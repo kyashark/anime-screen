5b2a39ab2c56739b788508f5984380af2ab7db01
shakya <152844822+kyashark@users.noreply.github.com>
2025-01-29 23:07:39 +0530

    Update

--- app/views/user/movieProfile.php
+++ app/views/user/movieProfile.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <p><?php echo $movie['movie_name'];  ?></p>
-    <p> <?php echo date('Y', strtotime($movie['release_date'])) ;?></p>
+    <p><?php echo date('Y', strtotime($movie['release_date'])) ;?></p>
     <p><?php echo $movie['genres']; ?></p>
     <p><?php echo $movie['description'];?></p>
     <p><?php echo $movie['image'];?></p>
